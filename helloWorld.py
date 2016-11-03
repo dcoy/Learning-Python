06493dbf6642a51829e764a4a10488626956bec8
Codeship dcoy <david@obsidianspork.io>
2016-11-03 12:49:26 -0500

    Multiline string

--- helloWorld.py
+++ helloWorld.py
@@ -1,5 +1,5 @@
 print("Hello, world.") # This is the print function
 # This is a single line comment -- the best comment
 
-''' this is a
-   multiline comment '''
+print(''' this is a
+   multiline string ''')
