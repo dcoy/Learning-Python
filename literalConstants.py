42f3164e6a043ff2fb927abb0b8db636b9bda20b
Codeship dcoy <david@obsidianspork.io>
2016-11-03 12:48:24 -0500

    Ins, vars, strings

---
+++ literalConstants.py
@@ -0,0 +1,22 @@
+# Literal Constants
+
+# Numbers are integers and floats
+# Instantiate integers and floats with variables
+integerExample = 2
+floatExample = 2.5
+
+# Print the variables!
+print(integerExample)
+print(floatExample)
+
+# Strings are literal constants
+
+# Instantiate variable with string -- bad naming ¯\_(ツ)_/¯
+
+stringVariable = "This is a string literal"
+stringVariable2 = "This is the second string literal"
+
+# Print the string variables!
+
+print(stringVariable)
+print(stringVariable2)
