5a743eea8f73b25f051ea657307c9e3562cf91dd
Codeship dcoy <david@obsidianspork.io>
2016-11-03 17:18:36 -0500

    Add verbose output with print statements for clarify

--- operators.py
+++ operators.py
@@ -1,2 +1,102 @@
+# Mathematical Operators
+
+# Addition
+print("This is the addition(+) operator")
+print('2 + 2')
 print(2 + 2)
+print("\n")
+
+# Subtraction
+print("This is the subtraction(-) operator")
+print('10 - 5')
+print(10 - 5)
+print("\n")
+
+# Multiplication
+print("This is the multiplication(*) operator")
+print('3 * 5')
 print(3 * 5)
+print("\n")
+
+# Division
+print("This is the division(/) operator")
+print('10 / 2')
+print(10 / 2)
+print("\n")
+
+# Power (exponential)
+print("This is the power(exponential) operator")
+print('10 ** 2')
+print(10 ** 2)
+print("\n")
+
+# Floor -- divide 'x' by 'y' and round down to the lowest number
+print("This is the floor(//) operator")
+print('10 // 3')
+print(10 // 3)
+print("\n")
+
+# Modulo -- divide 'x' by 'y' and print the remainder
+print("This is the modulo(%) operator")
+print('10 % 3')
+print(10 % 3)
+print("\n")
+# Less than -- provides a boolean(true or false) result
+print("This is the less than(<) operator")
+print('10 < 3')
+print(10 < 3)
+print("\n")
+
+# Greater than -- provides a boolean result
+print("This is the greater than(>) operator")
+print('10 > 2')
+print(10 > 2)
+print("\n")
+
+# Greater than or equal to -- provides a boolean result
+print("This is the greater than or equal to(>=) operator")
+print('9 >= 5')
+print(9 >= 5)
+print("\n")
+
+# Less than or equal to -- provides a boolean result
+print("This is the less than or equal to(<=) operator")
+print('5 <= 9')
+print(5 <= 9)
+print("\n")
+
+# Strictly equal to -- does not instanticate variable declaration -- provides boolean output
+print("This is the strictly equal to(==) operator")
+print('9 == 2')
+print(9 == 2)
+print("\n")
+
+# Not equal to -- provides boolean output
+print("This is the not equal to(!=) operator")
+print('12 != 12')
+print("\n")
+
+# Not operator -- checks the value of 'x';  If 'x' is True, return False.  If 'x' is False, return True
+print("This is the not operator(not) operator")
+print('x = True')
+x = True
+print(not x)
+print("\n")
+
+# And operator -- 'x' and 'y' return False if 'x' is False, else it returns eval. of y
+print("This is the and(and) operator")
+print('x = False')
+print('y = True')
+x = False
+y = True
+print(x and y)
+print("\n")
+
+# Or operator -- If 'x' is true, it will return True.  If 'y' is true, it will return True
+print("This is the or(or) operator")
+print('x = True')
+print('y = False')
+x = True
+y = False
+print(x or y)
+print (y or x)
