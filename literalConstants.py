b832ec3bcde5fa55bf958c1cc72b17cb212fff92
Codeship dcoy <david@obsidianspork.io>
2016-11-03 12:51:30 -0500

    Remove unnecessary carriage return

--- literalConstants.py
+++ literalConstants.py
@@ -4,19 +4,20 @@
 # Instantiate integers and floats with variables
 integerExample = 2
 floatExample = 2.5
+newFloatExample = integerExample + floatExample
+
 
 # Print the variables!
 print(integerExample)
 print(floatExample)
+print(newFloatExample)
 
 # Strings are literal constants
 
 # Instantiate variable with string -- bad naming ¯\_(ツ)_/¯
-
 stringVariable = "This is a string literal"
 stringVariable2 = "This is the second string literal"
 
 # Print the string variables!
-
 print(stringVariable)
 print(stringVariable2)
