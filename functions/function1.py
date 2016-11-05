6aaef515dd1ad7ae94a70535c49bc4ed2883a741
Codeship dcoy <david@obsidianspork.io>
2016-11-05 13:44:57 -0500

    Simple example function

---
+++ functions/function1.py
@@ -0,0 +1,8 @@
+# How this works: define the function name, 'say_hello', accepting no paramenters
+# then execute the block statement of `print 'Hello, world.'`
+# Finally, call the function as `say_hello()`.
+
+def say_hello():
+    print('Hello, world.')
+
+say_hello()
