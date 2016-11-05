29a998918a2fce6c1588440542bda2c49999a8b5
Codeship dcoy <david@obsidianspork.io>
2016-11-05 13:55:56 -0500

    Exploring local variable in functions

---
+++ functions/function_local.py
@@ -0,0 +1,12 @@
+# This will demonstrate a function variables within the local scope.
+
+# Global variable declaration
+x = 50
+
+def func(x):
+    print('x is', x)
+    x = 2
+    print('Changed local x to', x)
+
+func(x)
+print('x is still', x)
