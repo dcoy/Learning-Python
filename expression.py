65f5b4b1aea16a2b49cdcb0fa1ec067cc0363c66
Codeship dcoy <david@obsidianspork.io>
2016-11-05 12:45:23 -0500

    Expression example

---
+++ expression.py
@@ -0,0 +1,13 @@
+# How this works: the 'length' and 'breadth' of the rectables are stored in variables by
+# the same name.  They're used to calculate the area of the rectangle with the help
+# of using an expression.  The result is stored in the 'area' variable and then
+# printed out with the 'print' function.  Python pretty-prints this inherently.
+
+# Declare those variables
+length = 5
+breadth = 2
+
+area = length * breadth
+
+print('The area is', area)
+print('The perimeter is', 2 * (length + breadth))
