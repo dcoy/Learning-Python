b2a916e6d2bb6c135ec5e2799c20c79af5e85991
Codeship dcoy <david@obsidianspork.io>
2016-11-05 13:12:02 -0500

    Exploring control flow with while

---
+++ while.py
@@ -0,0 +1,26 @@
+# This file will demonstrate the use of the 'while' statement. It will show
+# the flow based on 'decisions' made by these statements
+
+# Using the 'while' statement -- while the condition is true, execute the block statements
+# 'else' process another block statement.  'Else' is optional.
+
+# Variable declaration
+number = 23
+running = True
+
+while running:
+    guess = int(input('Enter an integer : '))
+
+    if guess == number:
+        print('Congratulations, you guess it!')
+        # while loop will stop
+        running = False
+    elif guess < number:
+        print('No, it is a bit higher')
+    else:
+        print('No, it is a bit lower')
+
+else:
+    print('This "loop" is finished!')
+
+print('Done')
