96262001e6437e7b9f209deb82189eec605556b4
Codeship dcoy <david@obsidianspork.io>
2016-11-07 22:05:23 -0600

    Creating and importing modules

---
+++ modules/myModule_demo.py
@@ -0,0 +1,6 @@
+# This will demonstrate the usage of modules by importing the 'myModule' module.
+
+import myModule
+
+myModule.say_hi()
+print('Version', myModule.__version__)
---
+++ modules/myModule_demo2.py
@@ -0,0 +1,6 @@
+# This will demonstrate the usage of modules by using the from...import syntax
+
+from myModule import say_hi, __version__
+
+say_hi()
+print('Version', __version__)
