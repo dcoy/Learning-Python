18a67e3be84bd4a8d1d90536cddcba92c9f74eba
Codeship dcoy <david@obsidianspork.io>
2016-11-08 12:44:18 -0600

    Removed print operations

--- dataStructures/ds_using_set.py
+++ dataStructures/ds_using_set.py
@@ -2,19 +2,14 @@
 
 names = set(['David','Alex', 'Brian', 'Eric'])
 
-print("\nprint('David' in names)")
 print('David' in names)
-
-print("\nprint('John') in names")
 print('John') in names
 
 namesC = names.copy()
 namesC.add('Mason')
-print("\nnamesC.issuperset(names)")
-namesC.issuperset(names)
 
-print("\nnames.remove('Alex')")
+print(namesC.issuperset(names))
+
 names.remove('Alex')
 
-print("\nnames & namesC")
 print(names & namesC) # OR names.intersection(namesC)
