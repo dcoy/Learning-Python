6853a30c0385c7364c236677949ddb90f9592a34
Codeship dcoy <david@obsidianspork.io>
2016-11-08 16:20:26 -0600

    Fixed missing comparison on line 26

--- problemSolving/backup_ver1.py
+++ problemSolving/backup_ver1.py
@@ -23,7 +23,7 @@ zip_command = 'zip -r {0} {1}'.format(target, ' '.join(source))
 print('Zip command is:')
 print(zip_command)
 print('Running:')
-if os.system(zip_command):
+if os.system(zip_command) == 0:
     print('Successful backup to', target)
 else:
     print('Backup FAILED')
