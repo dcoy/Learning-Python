f799c8c515de250c81375f3bdb37f00919b17eb0
Codeship dcoy <david@obsidianspork.io>
2016-11-07 22:40:34 -0600

    Add len to zoo

--- dataStructures/ds_using_tuple.py
+++ dataStructures/ds_using_tuple.py
@@ -2,8 +2,7 @@
 # optional. Explicit is better than implicit.
 
 zoo = ('python', 'elephant', 'tigers')
-print('Number of animals in the zoo is', zoo)
-
+print('Number of animals in the zoo is', len(zoo))
 new_zoo = 'kangaroo', 'camel', zoo
 print('Number of cages in the new zoo is', len(zoo))
 print('All animals in the new zoo are', new_zoo)
