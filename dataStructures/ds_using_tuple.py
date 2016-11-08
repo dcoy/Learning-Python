81d642b70353030bfe77300ec224cfc44ebb343d
Codeship dcoy <david@obsidianspork.io>
2016-11-07 22:39:01 -0600

    Exploring the tuple data structure

---
+++ dataStructures/ds_using_tuple.py
@@ -0,0 +1,13 @@
+# It's recommended to always start/end a tuple with parentheses, even though they're
+# optional. Explicit is better than implicit.
+
+zoo = ('python', 'elephant', 'tigers')
+print('Number of animals in the zoo is', zoo)
+
+new_zoo = 'kangaroo', 'camel', zoo
+print('Number of cages in the new zoo is', len(zoo))
+print('All animals in the new zoo are', new_zoo)
+print('Animals bought from old zoo are', new_zoo[2])
+print('Last animal bought from old zoo is', new_zoo[2][2])
+print('Number of animals in the new zoo is',
+       len(new_zoo)-1+len(new_zoo[2]))
