932b6341f0dc1a1d9b956ae91eddf8c78f47c543
David Coy <david@obsidianspork.io>
2016-11-15 23:00:35 -0600

    Printing spam

--- functions/function_exercise_3-2.py
+++ functions/function_exercise_3-2.py
@@ -1,4 +1,5 @@
 # This is an exercise for learning about 'functions'
+
 # This function will take any word and 'right_justify' it
 # so the last letter is at column 70
 
@@ -9,3 +10,21 @@ def right_justify(s):
     print(s_rght * ' ' + s)
 
 right_justify("David")
+
+# Time to print some spam
+
+def do_twice(func, arg):
+    func(arg)
+    func(arg)
+
+def print_twice(arg):
+    print(arg)
+    print(arg)
+
+def do_four(func, arg):
+    do_twice(func, arg)
+    do_twice(func, arg)
+
+do_twice(print_twice, 'spam')
+print('\n')
+do_four(print_twice, 'spam')
