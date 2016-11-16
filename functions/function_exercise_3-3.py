572f4c99e41757d1a8dfd4cfa517ddfaf4942df0
David Coy <david@obsidianspork.io>
2016-11-16 12:46:24 -0600

    More 4x4 success

--- functions/function_exercise_3-3.py
+++ functions/function_exercise_3-3.py
@@ -44,3 +44,48 @@ def print_grid():
     print_beams()
 
 print_grid()
+
+# 4x4 grid time
+
+def one_four_one(f, g, h):
+    f()
+    do_four(g)
+    h()
+
+def print_plus():
+    print('+', end=' ')
+
+def print_dash():
+    print('-', end=' ')
+
+def print_bar():
+    print('|', end=' ')
+
+def print_space():
+    print(' ', end=' ')
+
+def print_end():
+    print()
+
+def nothing():
+    "do nothing"
+
+def print1beam():
+    one_four_one(nothing, print_dash, print_plus)
+
+def print1post():
+    one_four_one(nothing, print_space, print_bar)
+
+def print4beams():
+    one_four_one(print_plus, print1beam, print_end)
+
+def print4posts():
+    one_four_one(print_bar, print1post, print_end)
+
+def print_row():
+    one_four_one(nothing, print4posts, print4beams)
+
+def print_grid_four():
+    one_four_one(print4beams, print_row, nothing)
+
+print_grid_four()
