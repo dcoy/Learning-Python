175925b596292122ac4318e880559323dde48fef
David Coy <david@obsidianspork.io>
2016-11-16 12:44:32 -0600

    4x4 success

--- functions/function_exercise_3-3.py
+++ functions/function_exercise_3-3.py
@@ -22,4 +22,25 @@ def do_four(f):
     do_twice(f)
 
 def print_beam():
+    print('+ - - - -', end='')
+
+def print_post():
+    print('|        ', end='')
+
+def print_beams():
+    do_twice(print_beam)
+    print('+')
+
+def print_posts():
+    do_twice(print_post)
     print('|')
+
+def print_row():
+    print_beams()
+    do_four(print_posts)
+
+def print_grid():
+    do_twice(print_row)
+    print_beams()
+
+print_grid()
