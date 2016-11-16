446e5c1a31c4da1f269982eb91ab930cf44250be
David Coy <david@obsidianspork.io>
2016-11-16 10:58:08 -0600

    Print it twice first

--- functions/function_exercise_3-3.py
+++ functions/function_exercise_3-3.py
@@ -1,18 +1,25 @@
 # This exercise will draw a 4x4 grid with '+' and '-'
+# like so:
+# + - - - - + - - - - +
+# |         |         |
+# |         |         |
+# |         |         |
+# |         |         |
+# + - - - - + - - - - +
+# |         |         |
+# |         |         |
+# |         |         |
+# |         |         |
+# + - - - - + - - - - +
 
+# Print twice first
+def do_twice(f):
+    f()
+    f()
 
+def do_four(f):
+    do_twice(f)
+    do_twice(f)
 
-def plus_dash_plus():
-    plus = print('+', end=' ')
-    lines = print('- ' * 4, end='')
-    plus_2 = print('+')
-
-def side_mid():
-    print('| ' + ' ' * 8 + '|')
-    print('| ' + ' ' * 8 + '|')
-    print('| ' + ' ' * 8 + '|')
-    print('| ' + ' ' * 8 + '|')
-
-plus_dash_plus()
-side_mid()
-plus_dash_plus()
+def print_beam():
+    print('|')
