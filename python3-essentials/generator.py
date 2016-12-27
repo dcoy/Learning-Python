7fe92d2e1e204fa4945fd648387436d809f57a70
David Coy <david@obsidianspork.io>
2016-12-27 16:54:51 -0600

    Quick start - Generator

---
+++ python3-essentials/generator.py
@@ -0,0 +1,19 @@
+# Generators
+
+def isPrime(n):
+    if n == 1:
+        return False
+    for x in range(2, n):
+        if n % x == 0:
+            return False
+    else:
+        return True
+
+def primes(n = 1):
+    while(True):
+        if isPrime(n): yield n
+        n +=1
+
+for n in primes():
+    if n > 100: break
+    print(n)
