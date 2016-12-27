7a5fb5db0bd8995c1957ef7f2f073d19f55b91a8
David Coy <david@obsidianspork.io>
2016-12-27 15:44:21 -0600

    Functions in Python

---
+++ python3-essentials/functions.py
@@ -0,0 +1,15 @@
+# Functions in Python
+
+def isPrime(n):
+    if n == 1:
+        print("1 is special")
+        return False
+    for x in range(2, n):
+        if n % x == 0:
+            print("{} equals {} x {}".format(n, x, n // x))
+            return False
+    else:
+        print(n, "is a prime number.")
+        return True
+for n in range(1, 20):
+    isPrime(n)
